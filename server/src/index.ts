ed04b417ff89d7ead4105fa97c99aa58797172c7
sarathkp <sarathkpreman@gmail.com>
2026-04-17 23:21:18 +0530

    fix CORS for Vercel frontend

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: [
+      "http://localhost:5173",
+      "https://gymmvp.vercel.app",
+    ],
+    credentials: true,
+  }
+));
 app.use(cookieParser());
 app.use(express.json());
 
